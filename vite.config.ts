09f999e427f504105523cbd617310749d711c16e
ydvsailendar <ydvsailendar@gmail.com>
2025-03-19 02:17:30 +0000

    Update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Change this to your repository name if you're using GitHub Pages, e.g., "/portfolio"
+  base: "/me/", // Use the repository name for GitHub Pages
 });
